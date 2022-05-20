7dc7c7cbeda50dd738f7a538712bc48c8f1c3c5b
tproduit <timothee.produit@gmail.com>
2022-05-20 15:36:08 +0200

    Ajout station de traitement dans autres installations

--- extension/views/autres_installations.sql
+++ extension/views/autres_installations.sql
@@ -96,7 +96,33 @@ CREATE OR REPLACE VIEW qwat_ch_aeptc.autres_installations AS
 		--1305, -- "détruit"
 		--1306, -- "projet"
 		--1307, -- "fictif"
-	) AND fk_watertype = 1502;
+	) AND fk_watertype = 1502
+UNION
+	SELECT COALESCE(network_element.identification, installation.id::character varying) AS "Identificateur",
+	installation.name AS "Nom",
+	'Installation de traitement' AS "Remarque",
+	pressurezone.name AS "Identificateur_de_la_partie_de_reseau",
+	'Autre'::character varying AS "Type",
+	'Oui' AS "Traitement",
+	st_force2d(node.geometry) AS "Geometrie"
+	FROM qwat_od.installation
+	LEFT JOIN qwat_od.treatment treatment ON installation.id = treatment.id
+	LEFT JOIN qwat_od.node ON installation.id = node.id
+	LEFT JOIN qwat_od.network_element network_element ON installation.id = network_element.id
+	LEFT JOIN qwat_od.pressurezone pressurezone ON node.fk_pressurezone = pressurezone.id
+	WHERE treatment.id is not null 
+	AND network_element.fk_status = IN (
+		--101, -- "autre"
+		102, -- "inconnu"
+		103, -- "à déterminer"
+		1301 -- "en service"
+		--1302, -- "hors service"
+		--1303, -- "désaffecté"
+		--1304, -- "abandonné"
+		--1305, -- "détruit"
+		--1306, -- "projet"
+		--1307, -- "fictif"
+	) AND installation.fk_watertype = 1502;
 
 GRANT SELECT, REFERENCES, TRIGGER ON TABLE qwat_ch_aeptc.autres_installations TO qwat_viewer;
 GRANT ALL ON TABLE qwat_ch_aeptc.autres_installations TO qwat_user;
